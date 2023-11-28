0287481a24eba6024fe5d543899864cb3647f993
Rish Saddiq <rishab.saddiq0607@gmail.com>
2023-11-28 11:51:16 +0000

    Add project files.

---
+++ Super calc/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        TextBox3 = New TextBox()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.AutoSize = True
+        Button1.BackColor = SystemColors.ControlDarkDark
+        Button1.Font = New Font("Microsoft Sans Serif", 16F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.Location = New Point(218, 413)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(100, 83)
+        Button1.TabIndex = 0
+        Button1.Text = "+"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.AutoSize = True
+        Button2.BackColor = SystemColors.ControlDarkDark
+        Button2.Font = New Font("Microsoft Sans Serif", 16F, FontStyle.Bold, GraphicsUnit.Point)
+        Button2.Location = New Point(324, 413)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(100, 83)
+        Button2.TabIndex = 1
+        Button2.Text = "-"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.AutoSize = True
+        Button3.BackColor = SystemColors.ControlDarkDark
+        Button3.Font = New Font("Microsoft Sans Serif", 16F, FontStyle.Bold, GraphicsUnit.Point)
+        Button3.Location = New Point(430, 413)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(100, 83)
+        Button3.TabIndex = 2
+        Button3.Text = "*"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.AutoSize = True
+        Button4.BackColor = SystemColors.ControlDarkDark
+        Button4.Font = New Font("Microsoft Sans Serif", 16F, FontStyle.Bold, GraphicsUnit.Point)
+        Button4.Location = New Point(536, 413)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(100, 83)
+        Button4.TabIndex = 3
+        Button4.Text = "/"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.Font = New Font("Microsoft Sans Serif", 16F, FontStyle.Bold, GraphicsUnit.Point)
+        Button5.ForeColor = Color.CornflowerBlue
+        Button5.Location = New Point(370, 529)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(112, 49)
+        Button5.TabIndex = 4
+        Button5.Text = "Reset"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(218, 244)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 100)
+        TextBox1.TabIndex = 5
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(347, 244)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 100)
+        TextBox2.TabIndex = 6
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(477, 287)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(25, 25)
+        Label1.TabIndex = 7
+        Label1.Text = "="
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(536, 244)
+        TextBox3.Multiline = True
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 100)
+        TextBox3.TabIndex = 8
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Black", 20F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(262, 28)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(344, 54)
+        Label2.TabIndex = 9
+        Label2.Text = "Super Calculator"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(11F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.DarkKhaki
+        ClientSize = New Size(909, 667)
+        Controls.Add(Label2)
+        Controls.Add(TextBox3)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Super Calc"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label2 As Label
+End Class
